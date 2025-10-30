014d95ed43b2914234ade006bb1693eb5f9f61a4
SarahMon-Hub <sarahmon@ymail.com>
2025-10-30 10:38:34 +0800

    Add files via upload

--- DeploymentPlan.ps1
+++ DeploymentPlan.ps1
@@ -1,8 +1,8 @@
 '初始化 Terraform'
 terraform init -upgrade
 '建立 Terraform 執行計劃'
-terraform plan -out main.tfplan
+terraform plan -out .\sudo\main.tfplan
 '套用 Terraform 執行計畫'
-terraform apply main.tfplan
+terraform apply .\sudo\main.tfplan
 '確認結果'
 terraform output -raw container_ipv4_address
--- DestroyPlan.ps1
+++ DestroyPlan.ps1
@@ -1,4 +1,4 @@
 '執行 terraform 計劃 並指定 destroy 旗標'
-terraform plan -destroy -out main.destroy.tfplan
+terraform plan -destroy -out .\sudo\main.destroy.tfplan
 '執行 terraform apply 來應用執行計劃'
-terraform apply main.destroy.tfplan
+terraform apply .\sudo\main.destroy.tfplan
